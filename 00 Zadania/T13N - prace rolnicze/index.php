f31cf15b8ae68ed422fffaa37fd3832ec6fd676c
matpakla <matpakla@gmail.com>
2024-01-05 12:28:56 +0100

    prace rolnicze

---
+++ 00 Zadania/T13N - prace rolnicze/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>T13N - prace rolnicze</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+
+</header>
+
+<!-- https://www.realtimecolors.com/?colors=e6ece2-0c1009-b1ce9d-527b36-74b946&fonts=Volkhov-Volkhov -->
+
+<footer>
+
+</footer>
+</body>
+</html>
